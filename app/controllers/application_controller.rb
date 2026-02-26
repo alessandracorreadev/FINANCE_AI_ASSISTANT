06ee16e76315f44e1d68593fa33d15eb3409f5a8
Alessandra Correa <bkpalecorrea@gmail.com>
2026-02-26 19:57:10 -0300

    redirecionando login para pagina de months

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  # Redireciona para o dashboard_path após o login
+  def after_sign_in_path_for(resource)
+    months_path # Substitua pelo path desejado (ex: projects_path, root_path)
+  end
 end
