d94cd93ccb50eb70a86a76274ab007bae18e09b7
paulo elisque <pcelisque@gmail.com>
2026-02-26 21:14:13 -0300

    show criado

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_25_003356) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_25_100000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.bigint "month_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["month_id"], name: "index_chats_on_month_id"
+  end
 
   create_table "months", force: :cascade do |t|
     t.string "month"
@@ -37,5 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_25_003356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "months"
   add_foreign_key "months", "users"
 end
